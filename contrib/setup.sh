83b7058cfbd9f82ab4ef2e0a593674386fd8c6ce
Josh <33360890+QuantumStr1der@users.noreply.github.com>
2024-11-03 20:57:50 -0500

    Update setup.sh

--- contrib/setup.sh
+++ contrib/setup.sh
@@ -177,7 +177,6 @@ sudo systemctl status --no-pager nginx
 
 if [[ "$1" != "--no-build" ]]; then
     [ -d ~/gpt-home ] && rm -rf ~/gpt-home
-    git clone https://github.com/judahpaul16/gpt-home ~/gpt-home
     cd ~/gpt-home
     echo "Checking if the container 'gpt-home' is already running..."
     if [ $(docker ps -q -f name=gpt-home) ]; then
